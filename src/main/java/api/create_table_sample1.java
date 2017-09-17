c1d787208a977a578129e678eb606502e78dce41
quchunhui <quchunhui@163.com>
2017-09-17 21:11:18 +0800

    update

--- src/main/java/api/create_table_sample1.java
+++ src/main/java/api/create_table_sample1.java
@@ -12,12 +12,13 @@ import org.apache.hadoop.hbase.client.ConnectionFactory;
 public class create_table_sample1 {
     public static void main(String[] args) throws Exception {
         Configuration conf = HBaseConfiguration.create();
-        conf.set("hbase.zookeeper.quorum", "192.168.1.80,192.168.1.81,192.168.1.82");
+        conf.set("hbase.zookeeper.quorum", "192.168.6.3,192.168.6.4,192.168.6.5");
         Connection connection = ConnectionFactory.createConnection(conf);
         Admin admin = connection.getAdmin();
 
         HTableDescriptor desc = new HTableDescriptor(TableName.valueOf("TEST1"));
         desc.setMemStoreFlushSize(2097152L);          //2M(默认128M)
+        desc.setMaxFileSize(10485760L);               //10M(默认10G)
 
         HColumnDescriptor family1 = new HColumnDescriptor(constants.COLUMN_FAMILY_DF.getBytes());
         family1.setTimeToLive(2 * 60 * 60 * 24);     //过期时间
@@ -28,11 +29,7 @@ public class create_table_sample1 {
         family2.setMaxVersions(3);                   //版本数
         desc.addFamily(family2);
 
-        try {
-            admin.createTable(desc);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        admin.createTable(desc);
         admin.close();
         connection.close();
     }
